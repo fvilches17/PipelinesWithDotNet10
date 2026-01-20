68974107a80d4bcef1d883b7fdb380a8bb1521e0
Francisco Vilches <f_vilches17@outlook.com>
2026-01-20 21:55:40 +1300

    Update Greet.ps1

--- .github/workflows/scripts/Greet.ps1
+++ .github/workflows/scripts/Greet.ps1
@@ -3,8 +3,17 @@ param (
     [string]$Name,
 
     [Parameter(Mandatory = $true)]
-    [int]$LuckyNum
+    [int]$LuckyNum,
+
+    [Parameter(Mandatory = $true)]
+    [bool]$DryRun
 )
 
 Write-Host "Hello, $Name!."
 Write-Host "Luck num is $LuckyNum"
+
+if ($DryRun) {
+    Write-Host "Is a dry run"
+} else {
+    Write-Host "Is not a dry run"
+}
