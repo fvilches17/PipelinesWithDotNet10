c79f71375a95effde4fbd7251ed0e90cfdd6b071
Francisco Vilches <f_vilches17@outlook.com>
2026-01-20 21:51:25 +1300

    Update Greet.ps1

--- .github/workflows/scripts/Greet.ps1
+++ .github/workflows/scripts/Greet.ps1
@@ -1,6 +1,10 @@
 param (
     [Parameter(Mandatory = $true)]
-    [string]$Name
+    [string]$Name,
+
+    [Parameter(Mandatory = $true)]
+    [int]$LuckyNum
 )
 
 Write-Host "Hello, $Name!."
+Write-Host "Luck num is $LuckNum"
